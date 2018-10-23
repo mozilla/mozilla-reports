4b15e26fdce6e94b0cd6181561f147882c7e34f2
Jeff Klukas <jeff@klukas.net>
2018-10-23 10:26:09 -0400

    Add render and serve scripts

--- script/remove-nav-all.sh
+++ script/remove-nav-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Move to the root of the git repository.
+cd $(git rev-parse --show-toplevel)
+
 for d in $(find $PWD -name '*.kp' -type d); do
     echo "d is $d"
     ./script/remove-nav.py "$d/rendered_from_kr.html" > "$d/index.html"
