b9210295fce22b38e53229d0c631610deb02bef8
Jeff Klukas <jeff@klukas.net>
2018-10-23 10:26:09 -0400

    Add render and serve scripts

---
+++ script/render.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Move to the root of the git repository.
+cd $(git rev-parse --show-toplevel)
+
+REPORT_DIRS=$(find . -name 'report.json' | awk -F/ '{print $2}' | grep -v 'stage' | grep -v 'site' | sort | uniq)
+
+rm -rf stage/ && mkdir -p stage/post
+
+# Copy in html content to stage/post directory.
+for d in $REPORT_DIRS; do
+    echo $d
+    cp -r $d stage/post/
+done
+
+# TODO: Copy in static resources to stage directory once we have them in the repo.
+# cp -r static/ stage/static/
+
+# Generate the site content using docere.
+python -m docere.cli render --knowledge-repo stage/ --outdir site/
---
+++ script/serve.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# For local testing; starts a local HTTP server to render the site/ directory.
+
+# Move to the root of the git repository.
+cd $(git rev-parse --show-toplevel)
+
+# Move to the site dir
+cd site/
+
+python3 -m http.server
